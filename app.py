854c1e9a03e6a7f738ad360df8766dbffcfa971e
Muhammadfahril585 <laodefahril00@gmail.com>
2025-06-12 21:32:37 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "7776046370:AAEZaKCCpy288MclyE9OzSBrSqVSn1Rex90"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Halo!")
+
+def main():
+    application = ApplicationBuilder().token(TOKEN).build()
+
+    start_handler = CommandHandler("start", start)
+    application.add_handler(start_handler)
+
+    application.run_webhook(
+        listen="0.0.0.0",
+        port=8443,
+        url_path=TOKEN,
+        webhook_url=f"https://RENDER_URL/{TOKEN}",
+    )
+
+if __name__ == "__main__":
+    main()
