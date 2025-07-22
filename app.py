eb0772a024edce0f22059bcd456fc5d3856c673c
Muhammadfahril585 <laodefahril00@gmail.com>
2025-07-22 16:42:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,36 @@
 from telegram import Update
 from telegram.ext import (
     ApplicationBuilder, CommandHandler, ContextTypes,
-    MessageHandler, CallbackQueryHandler, ConversationHandler, filters
+    MessageHandler, CallbackQueryHandler, ConversationHandler, JobQueue, filters
 )
 from handlers.callbacks import handle_callback
 from handlers.start import start
 from handlers.tentang_kami import handle_tentang_kami
 from handlers.profil_pondok import handle_profil_pondok
-from handlers.visi_misi import handle_visi_misi   
+from handlers.visi_misi import handle_visi_misi
 from handlers.struktur_organisasi import handle_struktur_organisasi
 from handlers.program_pendidikan import handle_program_pendidikan
 from handlers.psb import handle_psb
 from handlers.unduh import handle_unduh
-from handlers.galeri import handle_galeri
-from handlers.layanan import handle_layanan
-from handlers.portal import handle_portal
+from handlers.edit_hafalan import edit_hafalan_massal_handler
 from handlers.daftar_halaqah import daftar_halaqah
-from handlers.lihat_santri import mulai_lihat_santri, handle_pilihan, tampilkan_santri_halaqah, PILIH_HALAQAH, TAMPIL_HALAQAH
 from handlers.tambah_santri import tambah_santri_handler
-from handlers.data_santri import detail_santri_handler
-from handlers.pendidikan import pendidikan_conv
-from handlers.kelas import kelas_handler
-from handlers.wali import wali_conv
-from handlers.tampilkan_data import lihat_data_handler
-from handlers.tampilkan_data import proses_nama
-from handlers.tampilkan_data import callback_nama
-from handlers.tampilkan_data import callback_lanjut_nama
-from handlers.angkatan import angkatan_handler
-from handlers.edit_hafalan import edit_hafalan_handler
-from handlers.edit_data import get_handler as get_edit_handler
-from handlers.edit_data_santri import get_edit_data_santri_handler
-from handlers.edit_pendidikan_santri import get_edit_pendidikan_santri_handler
-from handlers.edit_wali_santri import get_edit_wali_santri_handler
-from handlers.edit_kelas_santri import get_edit_kelas_santri_handler
-from handlers.edit_angkatan_santri import get_edit_angkatan_santri_handler
-from handlers.tandai_alumni import tandai_alumni, simpan_alumni
-from handlers.lihat_alumni import tampilkan_tahun_alumni, tampilkan_daftar_alumni_per_tahun
-from handlers.lapor_pekanan import lapor_handler
-from handlers.upload_foto import upload_foto_handler
-from handlers.compress import get_compress_handler
+from handlers.edit_laporan import get_conv_handler
+from handlers.galeri import handle_galeri
+from handlers.tambah_halaqah import tambah_halaqah_handler
+from handlers.rekap_bulanan import rekap_bulanan_handlers
+from handlers.start import cek_mode
+from handlers.layanan import handle_layanan
+from handlers.lapor_pekanan2 import laporan_pekanan_conv
+from handlers.portal import handle_portal
+from handlers.ai_handler import handle_ai_mode
+from handlers.lihat_santri import mulai_lihat_santri, detail_santri
+from handlers.pindah_santri import pindah_handler
+from handlers.edit_halaqah import edit_nama_halaqah_handler
+from handlers.start import set_mode
 from handlers.rubah_nama import rubah_nama_handler
-from handlers.rekapbulanan import (
-    handle_rekapbulanan_dinamis,
-    handle_pilih_bulan,
-    handle_pilih_halaqah
-)
+from handlers.admin import handle_admin
+from handlers.hapus_santri import hapus_handler
 import os
 import threading
 from flask import Flask
