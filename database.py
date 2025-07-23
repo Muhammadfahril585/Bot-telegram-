ec7d51f639141b575bb47cd2d0f8c443ac909e23
Muhammadfahril585 <laodefahril00@gmail.com>
2025-07-23 12:30:09 +0800

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-def get_db():
-    return psycopg2.connect(
-        host="dpg-d15ncqmuk2gs73fiulpg-a.oregon-postgres.render.com",
-        database="halaqah",
-        user="halaqah_user",
-        password="Iep6nWQ2GMJ3wVL3ntqkYte352qX7tu1"
-    )
