285c049020bc243c104bbb7e4a35c101951058d7
Muhammadfahril585 <laodefahril00@gmail.com>
2025-07-23 12:38:30 +0800

    Update callbacks.py

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 from handlers.daftar_halaqah import daftar_halaqah
-from handlers.rekapbulanan import handle_rekapbulanan_dinamis
+#from handlers.rekapbulanan import handle_rekapbulanan_dinamis
 from handlers.start import start
 
 async def handle_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -13,9 +13,9 @@ async def handle_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await query.edit_message_text("Menampilkan daftar halaqah...")
         await daftar_halaqah(update, context)
 
-    elif callback_data == 'rekap_bulanan':
-        await query.answer()
-        await handle_rekapbulanan_dinamis(update, context)
+    #elif callback_data == 'rekap_bulanan':
+        #await query.answer()
+       # await handle_rekapbulanan_dinamis(update, context)
 
     elif callback_data == 'lihat_santri':
         await query.answer()
