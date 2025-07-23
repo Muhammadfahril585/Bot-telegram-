f3a255b89b9c473f9bb6d06fde947129a2d1e445
Muhammadfahril585 <laodefahril00@gmail.com>
2025-07-23 15:01:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import os
 from telegram import Update
 from telegram.ext import (
     ApplicationBuilder, CommandHandler, ContextTypes,
     MessageHandler, CallbackQueryHandler, ConversationHandler, JobQueue, filters
 )
+from aiohttp import web
 from handlers.callbacks import handle_callback
 from handlers.start import start
 from handlers.tentang_kami import handle_tentang_kami
@@ -24,28 +26,18 @@ from handlers.lapor_pekanan2 import handle_reset_callback
 from handlers.ai_handler import handle_ai_mode
 from handlers.lihat_santri import mulai_lihat_santri, detail_santri
 from handlers.start import set_mode
-import os
-import threading
-from flask import Flask
+
 
 TOKEN = "7776046370:AAEZaKCCpy288MclyE9OzSBrSqVSn1Rex90"
-flask_app = Flask(__name__)
+PORT = int(os.environ.get("PORT", 10000))
+WEBHOOK_URL = f"https://bot-telegram-02rg.onrender.com/{TOKEN}"
 
-@flask_app.route('/')
-def home():
-    return 'Bot Telegram aktif.'
+async def ping(request):
+    return web.Response(text="pong")
 
-@flask_app.route('/ping')
-def ping():
-    return 'pong'
-def run_flask():
-    flask_app.run(host="0.0.0.0", port=8080)
-
-def main():
-    threading.Thread(target=run_flask).start()
-
+async def main():
     application = ApplicationBuilder().token(TOKEN).build()
-
+    
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CallbackQueryHandler(handle_tentang_kami, pattern="^tentang$"))
     application.add_handler(CallbackQueryHandler(handle_profil_pondok, pattern="^profil_pondok$"))
@@ -65,7 +57,7 @@ def main():
     application.add_handler(CallbackQueryHandler(handle_buat_pdf_rekap, pattern="^buat_pdf_rekap$"))
     application.add_handler(CallbackQueryHandler(handle_pertanyaan_konfirmasi, pattern="^pertanyaan_pondok_"))
     for handler in rekap_bulanan_handlers:
-      application.add_handler(handler)
+        application.add_handler(handler)
     application.add_handler(laporan_pekanan_conv)
     application.add_handler(CommandHandler("lihat_santri", mulai_lihat_santri))
     application.add_handler(CommandHandler("daftar_halaqah", daftar_halaqah))
@@ -74,12 +66,16 @@ def main():
 
     application.add_handler(CallbackQueryHandler(handle_callback))
     
-    application.run_webhook(
-      listen="0.0.0.0",
-      port=int(os.environ.get('PORT', 10000)),  # render akan otomatis pakai ini
-      url_path=TOKEN,
-      webhook_url=f"https://bot-telegram-02rg.onrender.com/{TOKEN}",
-)
+    app = application.web_app
+    app.router.add_get("/ping", ping)
+
+    await application.run_webhook(
+        listen="0.0.0.0",
+        port=PORT,
+        url_path=TOKEN,
+        webhook_url=WEBHOOK_URL,
+    )
 
 if __name__ == "__main__":
-    main()
+    import asyncio
+    asyncio.run(main())
