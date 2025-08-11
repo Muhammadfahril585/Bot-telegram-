9956308f6f354601cd8ea4b5453475d9a0661d36
Muhammadfahril585 <laodefahril00@gmail.com>
2025-08-11 12:19:48 +0800

    Update jadwal_sholat.py

--- handlers/jadwal_sholat.py
+++ handlers/jadwal_sholat.py
@@ -1,4 +1,5 @@
 import requests
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes, CallbackQueryHandler
